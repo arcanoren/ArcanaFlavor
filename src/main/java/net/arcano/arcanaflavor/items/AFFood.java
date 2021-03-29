162cb72bf1d9a74e4ada2e45cfd7f071083b5205
Arcano <ren.cunha@hotmail.com>
2021-03-29 02:08:46 -0300

    Add Cooked Apple to AFFood class

--- src/main/java/net/arcano/arcanaflavor/items/AFFood.java
+++ src/main/java/net/arcano/arcanaflavor/items/AFFood.java
@@ -1,8 +1,5 @@
 package net.arcano.arcanaflavor.items;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.block.SugarCaneBlock;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,4 +9,6 @@ public class AFFood {
             .food(new FoodComponent.Builder().hunger(6).saturationModifier(7).build()));
     public static final Item COOKED_BEETROOT = new Item(new Item.Settings().group(ItemGroup.FOOD)
             .food(new FoodComponent.Builder().hunger(4).saturationModifier(5).build()));
+    public static final Item COOKED_APPLE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(7).saturationModifier(6).build()));
 }
