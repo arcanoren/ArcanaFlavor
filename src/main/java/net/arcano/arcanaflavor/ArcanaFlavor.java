df1668d4fd6b1ad486bb27d8825212c3c2622135
Arcano <ren.cunha@hotmail.com>
2021-03-29 00:50:42 -0300

    Workspace Setup
    Create AFFood

---
+++ src/main/java/net/arcano/arcanaflavor/ArcanaFlavor.java
@@ -0,0 +1,10 @@
+package net.arcano.arcanaflavor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArcanaFlavor implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/arcano/arcanaflavor/items/AFFood.java
@@ -0,0 +1,15 @@
+package net.arcano.arcanaflavor.items;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class AFFood {
+    public static final Item COOKED_CARROT = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(6).saturationModifier(7).build()));
+    public static final Item COOKED_BEETROOT = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(4).saturationModifier(5).build()));
+}
