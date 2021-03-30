e3a939be9234c92bfb0eb44e11c99e006b05ad31
Arcano <ren.cunha@hotmail.com>
2021-03-30 01:56:11 -0300

    Add Pies: Apple, Beetroot, Chicken, Cod, Mushroom, Sweet Berry, Vegetable and Wart. Foods and Recipes.

--- src/main/java/net/arcano/arcanaflavor/items/AFFood.java
+++ src/main/java/net/arcano/arcanaflavor/items/AFFood.java
@@ -18,4 +18,23 @@ public class AFFood {
             .food(new FoodComponent.Builder().hunger(6).saturationModifier(7).build()));
     public static final Item COOKED_PUMPKIN_SLICE = new Item(new Item.Settings().group(ItemGroup.FOOD)
             .food(new FoodComponent.Builder().hunger(4).saturationModifier(2).build()));
+
+    //Pies
+    public static final Item APPLE_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(9).saturationModifier(6).build()));
+    public static final Item BEETROOT_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(7).saturationModifier(5.6F).build()));
+    public static final Item CHICKEN_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(11).saturationModifier(8).build()));
+    public static final Item CODFISH_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(7).saturationModifier(6).build()));
+    public static final Item MUSHROOM_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(8).saturationModifier(8).build()));
+    public static final Item SWEET_BERRY_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(6).saturationModifier(4).build()));
+    public static final Item VEGETABLE_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(12).saturationModifier(5.7F).build()));
+    public static final Item WART_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD)
+            .food(new FoodComponent.Builder().hunger(8).saturationModifier(4.8F).build()));
+
 }
--- src/main/java/net/arcano/arcanaflavor/registry/AFItems.java
+++ src/main/java/net/arcano/arcanaflavor/registry/AFItems.java
@@ -18,5 +18,15 @@ public class AFItems {
         Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "cooked_beetroot"), AFFood.COOKED_BEETROOT);
         Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "cooked_carrot"), AFFood.COOKED_CARROT);
         Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "cooked_pumpkin_slice"), AFFood.COOKED_PUMPKIN_SLICE);
+
+        //Pies
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "apple_pie"), AFFood.APPLE_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "beetroot_pie"), AFFood.BEETROOT_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "chicken_pie"), AFFood.CHICKEN_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "codfish_pie"), AFFood.CODFISH_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "mushroom_pie"), AFFood.MUSHROOM_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "sweet_berry_pie"), AFFood.SWEET_BERRY_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "vegetable_pie"), AFFood.VEGETABLE_PIE);
+        Registry.register(Registry.ITEM, new Identifier(ArcanaFlavor.MOD_ID, "wart_pie"), AFFood.WART_PIE);
     }
 }
